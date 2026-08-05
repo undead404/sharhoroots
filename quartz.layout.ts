0d6e78baf6e30ddfe83d7917d6ede3fa58ffc2c5
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2026-08-05 10:15:40 +0300

    better explorer sort

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,7 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
-import PlacesMap from "./quartz/components/PlacesMap"
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
+import PlacesMap from "./quartz/components/PlacesMap";
+import type { Options } from "./quartz/components/Explorer";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -9,10 +10,33 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Генеалогія Шаргородщини на Facebook": "https://www.facebook.com/groups/1748115539548381",
     },
   }),
+};
+
+const customSortFn: Options["sortFn"] = (a, b) => {
+  const orderMap: Record<string, number> = {
+    'map': 0,
+    'author': 1,
+    'settlement/index': 2,
+    'registrar/index': 3,
+    'administrative-division/index': 4
+  };
+
+  const orderA = a.slug in orderMap ? orderMap[a.slug] : Infinity;
+  const orderB = b.slug in orderMap ? orderMap[b.slug] : Infinity;
+
+  if (orderA !== orderB) {
+    return orderA - orderB;
+  }
+
+  console.log(b.displayName, a.displayName)
+
+  return a.displayName.localeCompare(b.displayName, 'uk', {
+    numeric: true,
+    sensitivity: "base",
+  });
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -40,18 +64,24 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      sortFn: customSortFn,
+    }),
   ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-}
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -67,4 +97,4 @@ export const defaultListPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [],
-}
+};
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,21 +1,28 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
+import {
+  QuartzComponent,
+  QuartzComponentConstructor,
+  QuartzComponentProps,
+} from "./types";
+import style from "./styles/footer.scss";
+import { version } from "../../package.json";
+import { i18n } from "../i18n";
 
 interface Options {
-  links: Record<string, string>
+  links: Record<string, string>;
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
+  const Footer: QuartzComponent = ({
+    displayClass,
+    cfg,
+  }: QuartzComponentProps) => {
+    const year = new Date().getFullYear();
+    const links = opts?.links ?? [];
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
@@ -25,9 +32,9 @@ export default ((opts?: Options) => {
           ))}
         </ul>
       </footer>
-    )
-  }
+    );
+  };
 
-  Footer.css = style
-  return Footer
-}) satisfies QuartzComponentConstructor
+  Footer.css = style;
+  return Footer;
+}) satisfies QuartzComponentConstructor;
